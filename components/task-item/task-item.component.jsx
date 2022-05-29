5964e6ed72e948298a04d0fbb3e089c35800342c
Ignacio Mansilla <ignaciomansilladeveloper@gmail.com>
2022-05-29 00:30:13 -0300

    task item styles and task form added

--- components/task-item/task-item.component.jsx
+++ components/task-item/task-item.component.jsx
@@ -3,9 +3,24 @@ import React from "react";
 
 export const TaskItem = ({ task }) => {
   return (
-    <View>
-      <Text>Title: {task.title}</Text>
+    <View style={style.taskItemContainer}>
+      <Text style={style.itemTitle}>TITLE: {task.title}</Text>
       <Text>Description: {task.description}</Text>
     </View>
   );
+};
+
+const style = {
+  taskItemContainer: {
+    backgroundColor: "#9575cd",
+    textAlign: "center",
+    padding: 20,
+    marginVertical: 8,
+    borderRadius: 10,
+  },
+  itemTitle: {
+    color: "#ffff",
+    fontWeight: "600",
+    fontSize: 16,
+  },
 };
--- screens/TaskFormScreen.jsx
+++ screens/TaskFormScreen.jsx
@@ -1,12 +1,75 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { TextInput, Text, TouchableOpacity, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { createTask } from "../api/api";
 
 import { Layout } from "../components/layout/layout.component";
 
-export const TaskFormScreen = () => {
+export const TaskFormScreen = ({ navigation }) => {
+  const [task, setTask] = useState({
+    title: "",
+    description: "",
+  });
+
+  const handleOnChangeText = (name, value) => {
+    setTask({ ...task, [name]: value });
+  };
+
+  const handleOnSubmit = () => {
+    if (task.title !== "") {
+      createTask(task);
+      navigation.navigate("Home");
+    }
+  };
+
   return (
     <Layout>
-      <Text>TaskFormScreen</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Title..."
+        placeholderTextColor="#546574"
+        maxLength={50}
+        onChangeText={(text) => handleOnChangeText("title", text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Description..."
+        placeholderTextColor="#546574"
+        multiline={true}
+        numberOfLines={10}
+        maxLength={100}
+        onChangeText={(text) => handleOnChangeText("description", text)}
+      />
+      <TouchableOpacity style={styles.buttonOnSave} onPress={handleOnSubmit}>
+        <Text style={styles.buttonText}>Create</Text>
+      </TouchableOpacity>
     </Layout>
   );
 };
+
+const styles = StyleSheet.create({
+  input: {
+    width: "90%",
+    marginVertical: 8,
+    fontSize: 16,
+    borderColor: "#ac108a",
+    borderWidth: 1,
+    height: 45,
+    color: "#fff",
+    textAlign: "center",
+    padding: 6,
+  },
+  buttonOnSave: {
+    width: "90%",
+    paddingTop: 10,
+    paddingBottom: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+    backgroundColor: "#65499cdb",
+  },
+  buttonText: {
+    textAlign: "center",
+    fontSize: 17,
+    color: "#fff",
+    fontWeight: "bold",
+  },
+});
