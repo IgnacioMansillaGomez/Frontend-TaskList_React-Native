e63b29c723637540e632ad1aff90f57515294042
Ignacio Mansilla <ignaciomansilladeveloper@gmail.com>
2022-05-29 00:28:37 -0300

    create task added

--- api/api.js
+++ api/api.js
@@ -4,3 +4,15 @@ export const getTasks = async () => {
   const res = await fetch(API);
   return await res.json();
 };
+
+export const createTask = async (newTask) => {
+  const res = await fetch(API, {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newTask),
+  });
+  return await res.json();
+};
